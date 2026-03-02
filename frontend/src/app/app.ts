7a5ed64e875ee9b4a43b89d048ba1bc1f3658212
Ahmed Dorgham <ahmeddorgham779@gmail.com>
2026-03-03 00:13:07 +0200

    feat:add hero-slider

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
+import { HeroSlider } from './layout/hero-slider/hero-slider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, HeroSlider],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
