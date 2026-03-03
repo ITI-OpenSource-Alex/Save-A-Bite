58457cc024d942271461c94afa9d9a836f38f2d0
Gliitchhh410 <ahmedmounir532@gmail.com>
2026-03-03 04:52:40 +0200

    feat: Category Cards and Section Layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
 import { HeroSlider } from './features/home/hero-slider/hero-slider';
 import { FlashDealsComponent } from './features/home/flash-deals/flash-deals';
+import { CategoriesSection } from './layout/categories-section/categories-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, HeroSlider, FlashDealsComponent],
+  imports: [RouterOutlet, Navbar, HeroSlider, FlashDealsComponent, CategoriesSection],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
