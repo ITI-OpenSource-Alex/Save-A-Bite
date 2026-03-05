9132806f74a190e50a9051f010fb12b484898e7b
AArek202 <abdelrahaman.amer202@gmail.com>
2026-03-05 22:20:34 +0200

    added routing configs

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,13 +4,15 @@ import orderRoutes from './order.routes';
 import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
 import categoryRoutes from './category.routes';
+import storeRoutes from "./store.routes";
 
 const rootRouter = Router();
 
-rootRouter.use('/auth', authRoutes);
-rootRouter.use('/addresses', addressRoutes);
-rootRouter.use('/orders', orderRoutes);
-rootRouter.use('/users', userRoutes);
-rootRouter.use('/categories', categoryRoutes);
+rootRouter.use("/auth", authRoutes);
+rootRouter.use("/addresses", addressRoutes);
+rootRouter.use("/orders", orderRoutes);
+rootRouter.use("/users", userRoutes);
+rootRouter.use("/categories", categoryRoutes);
+rootRouter.use("/stores", storeRoutes);
 
 export default rootRouter;
--- backend/src/routes/store.routes.ts
+++ backend/src/routes/store.routes.ts
@@ -29,3 +29,5 @@ router.delete(
   IsAuthenticatedMiddleware,
   storeController.deleteStoreById,
 );
+
+export default router;
